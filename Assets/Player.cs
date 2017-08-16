ce70d336e159c4b1d5d2a0f62126621b5ed82545
Jose Aponte <joseapontec@gmail.com>
2017-08-15 23:13:04 -0500

    Initial import with the Material Property Block Problem

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator anim;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        rend = GetComponent<Renderer>();
+
+        rend.material.color = Color.red;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)){
+            anim.SetFloat("changeColor", 1.0f);
+        }
+        else{
+            anim.SetFloat("changeColor", -1.0f);
+        }
+	}
+}
